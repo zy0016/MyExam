910dd4b82bdd3cd73aa0efd4b91bda46ce571a9a
zhaoyong <yong.zhao@hcl.com>
2018-10-18 09:38:23 +0800

    tree

--- exam/exam/exam/examtree3.cpp
+++ exam/exam/exam/examtree3.cpp
@@ -77,91 +77,6 @@ using namespace std;
 //		CreateNode(str);//构建二叉树
 //	}
 //
-//
-//	//先序遍历非递归算法
-//	void Disp()
-//	{
-//		if (t == NULL)
-//		{
-//			return;
-//		}
-//		stack<Node *> m_stack;//定义栈
-//		m_stack.push(t);
-//		while (!m_stack.empty())
-//		{
-//			Node *p = m_stack.top();//赋值一份当前双亲节点
-//			cout << p->data << ends;
-//			m_stack.pop();
-//			if (p->rchild)//先存储右子树，确保先输出左子树
-//			{
-//				m_stack.push(p->rchild);
-//			}
-//			if (p->lchild)//后存储左子树
-//			{
-//				m_stack.push(p->lchild);
-//			}
-//		}
-//
-//	}
-//
-//	//非递归中序遍历二叉树
-//	void DispMid()
-//	{
-//		if (t == NULL)
-//		{
-//			return;
-//		}
-//		Node *p = t;
-//		stack<Node *>m_stack;
-//		while (p != NULL || !m_stack.empty())
-//		{
-//			while (p != NULL)//一路直走至左下角
-//			{
-//				m_stack.push(p);
-//				p = p->lchild;
-//			}
-//			if (!m_stack.empty())
-//			{
-//				p = m_stack.top();//备份当前栈顶地址
-//				m_stack.pop();
-//				cout << p->data << ends;
-//				p = p->rchild;
-//			}
-//		}
-//	}
-//
-//	//非递归后序遍历二叉树
-//	void DispBehid()
-//	{
-//		if (t == NULL)
-//		{
-//			return;
-//		}
-//		Node *pre = NULL, *p = t;
-//		stack<Node *>m_stack;
-//		while (p != NULL || !m_stack.empty())
-//		{
-//			while (p != NULL)//一路直走至左下角
-//			{
-//				m_stack.push(p);
-//				p = p->lchild;
-//			}
-//			p = m_stack.top();
-//			//右子树为空或者已访问，输出当前节点
-//			if (p->rchild == NULL || p->rchild == pre)
-//			{
-//				cout << p->data << ends;
-//				pre = p;//将当前结点地址赋值pre作为下一次判断标志，防止重复访问
-//				m_stack.pop();
-//				p = NULL;//p赋值空以便访问右子树
-//			}
-//			else
-//			{
-//				p = p->rchild;//访问子树的右子树
-//			}
-//		}
-//	}
-//
 //	//层次遍历
 //	void level_display()
 //	{
@@ -187,44 +102,6 @@ using namespace std;
 //		}
 //	}
 //
-//	//递归先序遍历输出二叉树
-//	void display()
-//	{
-//		cout << "recursive preorder:";
-//		output(t);
-//		cout << endl;
-//	}
-//
-//	//递归中序遍历输出二叉树
-//	void displayMid()
-//	{
-//		cout << "recursive middle order:";
-//		outputMid(t);
-//		cout << endl;
-//	}
-//
-//	//递归后序遍历输出二叉树
-//	void displayBhind()
-//	{
-//		cout << "recursive bottom order";
-//		outputBhind(t);
-//		cout << endl;
-//	}
-//	//二叉树高度
-//	void Height()
-//	{
-//		int height = get_height(t);
-//		cout << "Height:" << height << endl;
-//	}
-//
-//	//输出叶子节点值
-//	void display_leaf()
-//	{
-//		cout << "Leaves: ";
-//		output_leaf(t);
-//		cout << endl;
-//	}
-//
 //	//查找二叉树中值data域为elem的节点
 //	void find_node(char elem)
 //	{
@@ -247,21 +124,6 @@ using namespace std;
 //		else
 //		{
 //			cout << "NO." << endl;
-//		}
-//	}
-//
-//	//计算节点数
-//	void nodes_count()
-//	{
-//		int sum;
-//		if (t == NULL)//若为空，则0个节点
-//		{
-//			sum = 0;
-//		}
-//		else
-//		{
-//			sum = node_count(t);
-//			cout << "Total Nodes:" << sum + 1 << endl;
 //		}
 //	}
 //private:
@@ -316,83 +178,6 @@ using namespace std;
 //			}
 //			//重构串，除去首字符，并将串长度减小1
 //			str.assign(str.substr(1, str.length() - 1));
-//		}
-//	}
-//
-//	//递归先序遍历输出二叉树
-//	void output(Node *t)
-//	{
-//		if (t != NULL)//当树根不为空时
-//		{
-//			cout << t->data;//输出
-//			if (t->lchild != NULL || t->rchild != NULL)//左/右结点不为空时递归到下一层
-//			{
-//				cout << "(";
-//				output(t->lchild);
-//				if (t->rchild != NULL)//当左节点遍历结束后，左节点递归返回一层，递归右节点
-//				{
-//					cout << ",";
-//				}
-//				output(t->rchild);
-//				cout << ")";
-//			}
-//		}
-//	}
-//
-//	//递归中序遍历二叉树
-//	void outputMid(Node *t)
-//	{
-//		if (t == NULL)//空则返回
-//		{
-//			return;
-//		}
-//		else
-//		{
-//			cout << "(";
-//			outputMid(t->lchild);//递归左孩子节点
-//			if (t->rchild != NULL)
-//			{
-//				cout << ",";
-//			}
-//			cout << t->data;//输出
-//			outputMid(t->rchild);//递归右孩子结点
-//			cout << ")";
-//		}
-//	}
-//
-//	//递归后序遍历输出二叉树
-//	void outputBhind(Node *t)
-//	{
-//		if (!t)//空则返回
-//		{
-//			return;
-//		}
-//		else
-//		{
-//			cout << "(";
-//			outputBhind(t->lchild);//递归左孩子节点
-//			if (t->rchild != NULL)
-//			{
-//				cout << ",";
-//			}
-//			outputBhind(t->rchild);//递归右孩子结点
-//			cout << t->data;//输出
-//			cout << ")";
-//		}
-//	}
-//	//求树高
-//	int get_height(Node *t)
-//	{
-//		int leftheight, rightheight;
-//		if (t == NULL)//递归至不存在子节点时返回0
-//		{
-//			return 0;
-//		}
-//		else
-//		{
-//			leftheight = get_height(t->lchild);//递归求左子树高度
-//			rightheight = get_height(t->rchild);//递归其右子树高度
-//			return leftheight > rightheight ? leftheight + 1 : rightheight + 1;//递归返回时返回最大值
 //		}
 //	}
 //
@@ -454,29 +239,7 @@ using namespace std;
 //		}
 //	}
 //
-//	//计算节点数
-//	int node_count(Node *t)
-//	{
-//		int lcount = 0, rcount = 0;
-//		if (t == NULL)//空则返回
-//		{
-//			return 0;
-//		}
-//		else
-//		{
-//			if (t->lchild != NULL)//遍历左孩子节点
-//			{
-//				lcount = node_count(t->lchild);
-//				lcount += 1;
-//			}
-//			if (t->rchild != NULL)//遍历右孩子节点
-//			{
-//				rcount = node_count(t->rchild);
-//				rcount += 1;
-//			}
-//			return lcount + rcount;//返回当前左右孩子节点数
-//		}
-//	}
+
 //};
 //
 //
@@ -525,9 +288,11 @@ void CreateBiTree(TreeNode* &T);
 void Inorder(TreeNode* &T);
 void PreOrderTraverse(TreeNode* &T);
 void Posorder(TreeNode* &T);
-void DispMid(TreeNode* &t);
-void Disp(TreeNode* &t);
-void DispBehid(TreeNode* &t);
+int DispMid(TreeNode* &t);
+int Disp(TreeNode* &t);
+int DispBehid(TreeNode* &t);
+int node_count(TreeNode *t);
+int get_height(TreeNode *t);
 //===========================================主函数
 int testmytree3_1() {
 	cout << "create a tree,the A->Z are data,with # is null:" << endl;
@@ -536,29 +301,32 @@ int testmytree3_1() {
 	PreOrderTraverse(T);
 	cout << endl;
 
-	Disp(T);
-	cout << endl;
+	int idisp = Disp(T);
+	cout <<"\tcount:"<<idisp<< endl;
 
 	cout << "middle order:" << endl;
 	Inorder(T);
 	cout << endl;
 
-	DispMid(T);
-	cout << endl;
+	int imid = DispMid(T);
+	cout <<"\tcount:"<<imid<< endl;
 
 	cout << "post order:" << endl;
 	Posorder(T);
 	cout << endl;
-	DispBehid(T);
-	cout << endl;
+	int ibehid = DispBehid(T);
+	cout <<"\tcount:"<<ibehid << endl;
+
+	cout << "tree height:"<< get_height(T) << endl;
 	return 1;
 }
 //先序遍历非递归算法
-void Disp(TreeNode* &t)
+int Disp(TreeNode* &t)
 {
+	int count = 0;
 	if (t == NULL)
 	{
-		return;
+		return 0;
 	}
 	stack<TreeNode *> m_stack;//定义栈
 	m_stack.push(t);
@@ -566,6 +334,7 @@ void Disp(TreeNode* &t)
 	{
 		TreeNode *p = m_stack.top();//赋值一份当前双亲节点
 		cout << p->data;
+		count++;
 		m_stack.pop();
 		if (p->rchild)//先存储右子树，确保先输出左子树
 		{
@@ -576,12 +345,14 @@ void Disp(TreeNode* &t)
 			m_stack.push(p->lchild);
 		}
 	}
+	return count;
 }
-void DispMid(TreeNode* &t)
+int DispMid(TreeNode* &t)
 {
+	int count = 0;
 	if (t == NULL)
 	{
-		return;
+		return 0;
 	}
 	TreeNode *p = t;
 	stack<TreeNode *>m_stack;
@@ -597,16 +368,19 @@ void DispMid(TreeNode* &t)
 			p = m_stack.top();//备份当前栈顶地址
 			m_stack.pop();
 			cout << p->data /*<< ends*/;
+			count++;
 			p = p->rchild;
 		}
 	}
+	return count;
 }
 //非递归后序遍历二叉树
-void DispBehid(TreeNode* &t)
+int DispBehid(TreeNode* &t)
 {
+	int count = 0;
 	if (t == NULL)
 	{
-		return;
+		return 0;
 	}
 	TreeNode *pre = NULL, *p = t;
 	stack<TreeNode *>m_stack;
@@ -622,6 +396,7 @@ void DispBehid(TreeNode* &t)
 		if (p->rchild == NULL || p->rchild == pre)
 		{
 			cout << p->data/* << ends*/;
+			count++;
 			pre = p;//将当前结点地址赋值pre作为下一次判断标志，防止重复访问
 			m_stack.pop();
 			p = NULL;//p赋值空以便访问右子树
@@ -630,6 +405,45 @@ void DispBehid(TreeNode* &t)
 		{
 			p = p->rchild;//访问子树的右子树
 		}
+	}
+	return count;
+}
+//计算节点数
+int node_count(TreeNode *t)
+{
+	int lcount = 0, rcount = 0;
+	if (t == NULL)//空则返回
+	{
+		return 0;
+	}
+	else
+	{
+		if (t->lchild != NULL)//遍历左孩子节点
+		{
+			lcount = node_count(t->lchild);
+			lcount += 1;
+		}
+		if (t->rchild != NULL)//遍历右孩子节点
+		{
+			rcount = node_count(t->rchild);
+			rcount += 1;
+		}
+		return lcount + rcount;//返回当前左右孩子节点数
+	}
+}
+//求树高
+int get_height(TreeNode *t)
+{
+	int leftheight, rightheight;
+	if (t == NULL)//递归至不存在子节点时返回0
+	{
+		return 0;
+	}
+	else
+	{
+		leftheight = get_height(t->lchild);//递归求左子树高度
+		rightheight = get_height(t->rchild);//递归其右子树高度
+		return leftheight > rightheight ? leftheight + 1 : rightheight + 1;//递归返回时返回最大值
 	}
 }
 //=============================================先序递归创建二叉树树
